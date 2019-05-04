98b122364e3c7de0a515e983587fd7d9f5d26499
Mindaugas Lazauskas <mind.lazauskas@gmail.com>
2019-05-04 17:02:53 +0300

    implemented delete functionality

--- posts/delete.js
+++ posts/delete.js
@@ -1,1 +1,22 @@
-module.exports.main = (event, context, body) => {};
+const AWS = require("aws-sdk");
+const responseConstructor = require("./responseConstructor");
+
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+module.exports.main = (event, context, callback) => {
+  const { id, username } = JSON.parse(event.body);
+  const res = responseConstructor(callback);
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: { id },
+    ConditionExpression: "#u = :u",
+    ExpressionAttributeNames: { "#u": "username" },
+    ExpressionAttributeValues: { ":u": username }
+  };
+
+  documentClient.delete(params, err => {
+    if (err) res.error(err, "Could not delete an item.");
+    else res.success(id);
+  });
+};
